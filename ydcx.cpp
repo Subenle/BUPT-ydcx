dceffe4ebd02db630c3349e1e82918f83dae122a
sublele <benle126@126.com>
2016-10-12 19:36:06 +0800

    update listWgt_time_energy

--- ydcx.cpp
+++ ydcx.cpp
@@ -16,7 +16,7 @@ ydcx::ydcx(QWidget *parent) :
     ui->tblWgt_goudian->setColumnWidth(3,60);
     ui->tblWgt_goudian->setColumnWidth(4,60);
     ui->tblWgt_goudian->setColumnWidth(5,60);
-
+    ui->listWgt_time_energy->addItem(u8"抄表时间\t剩余电量");
     QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
     setWindowTitle(tr(u8"北邮用电查询"));
     connect(ui->bt_search,SIGNAL(clicked()),this,SLOT(bt_search_clicked()));
